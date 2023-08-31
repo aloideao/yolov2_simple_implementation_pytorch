b41a4d1df23aac2daaaa591f32517bed9ab1948a
aloideao <121831118+aloideao@users.noreply.github.com>
2023-08-31 16:04:33 -0400

    Update train.py

--- train.py
+++ train.py
@@ -12,7 +12,7 @@ import torchvision
 from utils.loss import * 
 from data.grid_creator import matcher
 from model.yolov2 import yolov2 
-
+from test import evaluate 
 #the most basic trainer ever 
 
 input_size=416
@@ -22,7 +22,10 @@ root=r"E:\b\yolov1_final\data\VOCdevkit"
 
 epochs=50
 model=yolov2(416)
+
+
 model.train().to(device)
+
 optimizer=torch.optim.SGD(model.parameters(),.001)
 scheduler=torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer,factor=.1,patience=10)
 
@@ -38,8 +41,20 @@ dataloader = torch.utils.data.DataLoader(
                     pin_memory=True
                     )
 
+validation_dataset = VOCDetection(data_dir=r'data/VOCdevkit',image_sets=[('2007', 'test')], img_size=416, transform=ValTransforms())
+validation_dataloader = torch.utils.data.DataLoader(
+    validation_dataset,
+    batch_size=8,
+    shuffle=False,
+    collate_fn=detection_collate_fn,
+    pin_memory=True
+)
+
+
 for epoch in range(epochs):
+    model.train()
     for j,(x,y) in enumerate(dataloader):
+        
         x=x.to(device)
         gt=matcher(y,13,13,model.anchors)
         pred=model(x)
@@ -47,9 +62,12 @@ for epoch in range(epochs):
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
-
+        
         if j % 50==0:
             print(lxy.item(),lwh.item(),lclass.item(),lconf.item(),loss.item())
+
+    scheduler.step(loss)
     print('epoch',epoch)
-    if (epoch+1)%10==0:
+    if (epoch)%10==0:
         torch.save(model.state_dict(),f'epoch_{epoch+1}')
+        val=evaluate(validation_dataloader,model,device=device)
